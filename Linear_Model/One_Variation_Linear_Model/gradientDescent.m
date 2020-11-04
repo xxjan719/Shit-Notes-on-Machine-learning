9da165221f2b27617323aae102eae160bae6863d
xxjan719 <62269887+xxjan719@users.noreply.github.com>
2020-11-04 15:52:58 +0800

    Update gradientDescent.m

--- Linear_Model/One_Variation_Linear_Model/gradientDescent.m
+++ Linear_Model/One_Variation_Linear_Model/gradientDescent.m
@@ -1,4 +1,11 @@
 function theta=gradientDescent(X,y,theta,alpha,num_iters)
+%https://www.jianshu.com/p/c7e642877b0e
+%https://blog.csdn.net/jizhidexiaoming/article/details/80386360?utm_medium=distribute.pc_relevant.none-task-blog-title-2&spm=1001.2101.3001.4242
+%https://blog.csdn.net/onthewaygogoing/article/details/68485682
+%https://blog.csdn.net/catherined/article/details/82015857
+%https://blog.csdn.net/catherined/article/details/81873721?utm_source=blogxgwz6&utm_medium=distribute.pc_relevant.none-task-blog-title-2&spm=1001.2101.3001.4242
+%https://blog.csdn.net/weixin_35732969/article/details/80506276?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-2.channel_param
+%https://blog.csdn.net/fair_li/article/details/106540284
 
 m=length(y);
 for iter =1:num_iters
