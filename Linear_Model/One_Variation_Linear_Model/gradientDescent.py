d58ce65c65d8c1c8236e457745b0f30d2c471a60
xxjan719 <62269887+xxjan719@users.noreply.github.com>
2020-11-05 09:50:19 +0800

    Update gradientDescent.py

--- Linear_Model/One_Variation_Linear_Model/gradientDescent.py
+++ Linear_Model/One_Variation_Linear_Model/gradientDescent.py
@@ -1,3 +1,4 @@
+#this code often is uesd to measure the multi-variartion regression gradient descent
 import numpy as np
 
 # Size of the points dataset.
@@ -7,7 +8,7 @@ m = 20
 X0 = np.ones((m, 1))
 X1 = np.arange(1, m+1).reshape(m, 1)
 X = np.hstack((X0, X1))
-#https://www.jianshu.com/p/c7e642877b0e
+
 # Points y-coordinate
 y = np.array([
     3, 4, 5, 5, 2, 4, 7, 8, 11, 8, 12,
@@ -16,25 +17,25 @@ y = np.array([
 
 # The Learning Rate alpha.
 alpha = 0.01
-
-def error_function(theta, X, y):
+class Gradient_desent:
+    def error_function(theta, X, y):
     '''Error function J definition.'''
     diff = np.dot(X, theta) - y
-    return (1./2*m) * np.dot(np.transpose(diff), diff)
-
-def gradient_function(theta, X, y):
+      return (1./2*m) * np.dot(np.transpose(diff), diff)
+    
+    def gradient_function(theta, X, y):
     '''Gradient of the function J definition.'''
     diff = np.dot(X, theta) - y
     return (1./m) * np.dot(np.transpose(X), diff)
 
-def gradient_descent(X, y, alpha):
-    '''Perform gradient descent.'''
-    theta = np.array([1, 1]).reshape(2, 1)
-    gradient = gradient_function(theta, X, y)
-    while not np.all(np.absolute(gradient) <= 1e-5):
-        theta = theta - alpha * gradient
-        gradient = gradient_function(theta, X, y)
-    return theta
+    def gradient_descent(X, y, alpha):
+       '''Perform gradient descent.'''
+       theta = np.array([1, 1]).reshape(2, 1)
+       gradient = gradient_function(theta, X, y)
+       while not np.all(np.absolute(gradient) <= 1e-5):
+          theta = theta - alpha * gradient
+          gradient = gradient_function(theta, X, y)
+      return theta
 
 optimal = gradient_descent(X, y, alpha)
 print('optimal:', optimal)
