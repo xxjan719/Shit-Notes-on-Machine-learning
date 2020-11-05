09f14e7734080c93aee0644551ab008354c79440
xxjan719 <62269887+xxjan719@users.noreply.github.com>
2020-11-05 12:30:55 +0800

    Update gradientDescent.py

--- Linear_Model/One_Variation_Linear_Model/gradientDescent.py
+++ Linear_Model/One_Variation_Linear_Model/gradientDescent.py
@@ -1,42 +1,50 @@
 #this code often is uesd to measure the multi-variartion regression gradient descent
-import numpy as np
 
-# Size of the points dataset.
-m = 20
+import numpy as np 
+import matplotlib.pyplot as plt 
 
-# Points x-coordinate and dummy value (x0, x1).
+# y = np.array([2,3,3,5,8,10,10,13,15,15,16,19,19,20,22,22,25,28])
+# x = np.arange(1,len(y)+1)
+# plt.figure()
+# plt.scatter(x,y)
+# plt.grid(True)
+# plt.show()
+
+# sample length
+m = 18
+
+# generate matrix X
 X0 = np.ones((m, 1))
 X1 = np.arange(1, m+1).reshape(m, 1)
 X = np.hstack((X0, X1))
 
-# Points y-coordinate
-y = np.array([
-    3, 4, 5, 5, 2, 4, 7, 8, 11, 8, 12,
-    11, 13, 13, 16, 17, 18, 17, 19, 21
-]).reshape(m, 1)
+# matrix y
+y = np.array([2,3,3,5,8,10,10,13,15,15,16,19,19,20,22,22,25,28]).reshape(m,1)
 
-# The Learning Rate alpha.
+# alpha
 alpha = 0.01
-class Gradient_desent:
-    def error_function(theta, X, y):
-    '''Error function J definition.'''
+
+def cost_function(theta, X, y):
     diff = np.dot(X, theta) - y
-      return (1./2*m) * np.dot(np.transpose(diff), diff)
-    
-    def gradient_function(theta, X, y):
-    '''Gradient of the function J definition.'''
+    return (1./2*m) * np.dot(np.transpose(diff), diff)
+
+def gradient_function(theta, X, y):
     diff = np.dot(X, theta) - y
     return (1./m) * np.dot(np.transpose(X), diff)
 
-    def gradient_descent(X, y, alpha):
-       '''Perform gradient descent.'''
-       theta = np.array([1, 1]).reshape(2, 1)
-       gradient = gradient_function(theta, X, y)
-       while not np.all(np.absolute(gradient) <= 1e-5):
-          theta = theta - alpha * gradient
-          gradient = gradient_function(theta, X, y)
-      return theta
+def gradient_descent(X, y, alpha):
+    theta = np.array([1, 1]).reshape(2, 1)
+    gradient = gradient_function(theta, X, y)
+    while not np.all(np.absolute(gradient) <= 1e-5):
+        theta = theta - alpha * gradient
+        gradient = gradient_function(theta, X, y)
+    return theta
 
-optimal = gradient_descent(X, y, alpha)
-print('optimal:', optimal)
-print('error function:', error_function(optimal, X, y)[0,0])
+[theta0, theta1] = gradient_descent(X, y, alpha)
+plt.figure()
+plt.scatter(X1,y)
+plt.plot(X1, theta0 + theta1*X1, color='r')
+plt.title('linear regression based on the gradient descent algorithm')
+plt.grid(True)
+plt.show()
+
